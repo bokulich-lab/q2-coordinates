5d94de8df5b71dc44dd3ba6c9a4e1168af449dd6
Zoe <zoe.challacombe@gmail.com>
2020-06-03 11:07:31 -0700

    working quadtree plugin

--- q2_coordinates/__init__1.py
+++
@@ -1,9 +0,0 @@
-import importlib
-
-from ._format import QuadTreeFormat, QuadTreeDirectoryFormat
-from ._type import SampleData, QuadTree
-
-__all__ = ['QuadTreeFormat', 'QuadTreeDirectoryFormat',
-           'SampleData', 'QuadTree']
-
-importlib.import_module('q2_types.sample_data._transformer')
