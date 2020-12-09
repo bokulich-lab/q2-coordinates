80017a134900334451c2d4859d38fec610e0bbe7
Zoe <zoe.challacombe@gmail.com>
2020-12-09 11:03:09 -0800

    flake8

--- q2_coordinates/tests/test_quadtree.py
+++ q2_coordinates/tests/test_quadtree.py
@@ -66,10 +66,14 @@ class BasicTest(unittest.TestCase):
         # set the correct tree
         self.correct_tree = skbio.TreeNode.read(
             StringIO(
-                "((('test_id_sw1':1.0)'3:1.0', ('test_id_sw2':1.0)'1:1.0')'3.:1.0',"
-                "(('test_id_nw1':1.0)'1:1.0', ('test_id_nw2':1.0)'3:1.0')'1.:1.0',"
-                "(('test_id_se1':1.0)'4:1.0', ('test_id_se2':1.0)'2:1.0')'4.:1.0',"
-                "(('test_id_ne1':1.0)'2:1.0', ('test_id_ne2':1.0)'4:1.0')'2.:1.0')root;"))
+                "((('test_id_sw1':1.0)'3:1.0'," 
+                "('test_id_sw2':1.0)'1:1.0')'3.:1.0',"
+                "(('test_id_nw1':1.0)'1:1.0',"
+                "('test_id_nw2':1.0)'3:1.0')'1.:1.0',"
+                "(('test_id_se1':1.0)'4:1.0',"
+                "('test_id_se2':1.0)'2:1.0')'4.:1.0',"
+                "(('test_id_ne1':1.0)'2:1.0',"
+                "('test_id_ne2':1.0)'4:1.0')'2.:1.0')root;"))
 
     # clean dataframe tests
     def test_clean_df(self):
